00f5a1ec267319b0f3c5d595ab2778e4532a58bf
Daniel Ruz <daniel.ruzvaz@gmail.com>
2018-06-28 16:35:30 +0200

    Facade working

--- AutoMapperFacadeReflection/FacadeSubManager.cs
+++ AutoMapperFacadeReflection/FacadeSubManager.cs
@@ -8,5 +8,16 @@ namespace AutoMapperFacadeReflection
 {
     public class FacadeSubManager
     {
+        public void TurnOffEverything()
+        {
+            string subMonitor = FacadeSub2.MonitorOff.TurnOffMonitor();
+            string subComputer = FacadeSub2.ComputerOff.TurnOffComputer();
+
+            string subServices = FacadeSub1.ServicesOff.TurnServicesOff();
+            string subWindows = FacadeSub1.WindowsOff.TurnWindowsOff();
+
+            Console.WriteLine(string.Format(@"{0}{1}{2}{3}{4}{5}{6}", subMonitor, Environment.NewLine,
+                subComputer, Environment.NewLine, subServices, Environment.NewLine, subWindows));
+        }
     }
 }
